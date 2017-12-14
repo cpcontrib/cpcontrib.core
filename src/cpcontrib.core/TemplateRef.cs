aa5a0bf34311044a18cdae894aa0879da42a6da9
Eric Newton <ericnewton76@gmail.com>
2017-12-14 14:12:50 -0500

    initial project commit

---
+++ src/cpcontrib.core/TemplateRef.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrownPeak.CMSAPI;
+
+//#namespace {generated}
+namespace cpcontrib.core
+{
+
+	/// <summary>
+	/// Provides a reference to a CrownPeak Template.
+	/// </summary>
+	public class TemplateRef
+	{
+
+		public TemplateRef(string assetPath, int templateId)
+		{
+			this._AssetPath = assetPath;
+			this._TemplateId = templateId;
+		}
+
+		string _AssetPath;
+		int _TemplateId;
+
+		public string AssetPath
+		{
+			get { return this._AssetPath; }
+		}
+
+		/// <summary>
+		/// The TemplateId or AssetId of the Template.
+		/// </summary>
+		public int TemplateId
+		{
+			get { return this._TemplateId; }
+		}
+
+	}
+
+}
