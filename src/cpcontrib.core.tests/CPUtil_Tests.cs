f09fa914d4d65642fcbc5582541546d9b12bf9b1
Eric Newton <ericnewton76@gmail.com>
2017-12-14 18:12:55 -0500

    added CPUtil.MakeDictionary

---
+++ src/cpcontrib.core.tests/CPUtil_Tests.cs
@@ -0,0 +1,64 @@
+﻿using CrownPeak.CMSAPI.Custom_Library;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+namespace cpcontrib.core.tests
+{
+	[TestFixture]
+	public class CPUtil_Tests
+	{
+
+
+		[Test]
+		public void MakeDictionary_duplicate_values_throws()
+		{
+			//arrange
+			string[] values = { "There", "There" };
+
+			//act
+			Assert.Throws<ArgumentException>(() =>
+			{
+				CPUtil.MakeDictionary(values);
+			});
+			//assert
+		}
+
+		[Test]
+		public void MakeDictionary_no_args_returns_empty()
+		{
+			//arrange 
+			string[] values = null;
+
+			//act
+			var actual = CPUtil.MakeDictionary(values);
+
+			//assert
+			actual.Should().NotBeNull();
+			actual.Should().BeEmpty();
+		}
+
+		[Test]
+		[TestCase("One,Two,Three")]
+		public void MakeDictionary_returns_keyvalue_paired(string valuesSeparated)
+		{
+			//arrange
+			string[] values = valuesSeparated.Split(',');
+
+			//act
+			var actual = CPUtil.MakeDictionary(values);
+
+			//assert
+			actual.ShouldAllBeEquivalentTo(values.Select(val => new KeyValuePair<string, string>(val, val)));
+
+
+
+		}
+
+	}
+}
---
+++ src/cpcontrib.core/CPUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownPeak.CMSAPI.Custom_Library
+{
+	public static class CPUtil
+	{
+
+
+		/// <summary>
+		/// Creates a Dictionary<string,string> from the supplied values.  Any duplicated values will cause an exception.</string>
+		/// </summary>
+		/// <param name="values"></param>
+		/// <returns></returns>
+		public static Dictionary<string, string> MakeDictionary(params string[] values)
+		{
+			Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+			if(values == null || values.Length == 0) return dictionary;
+
+			foreach(string value in values)
+			{
+				dictionary.Add(value, value);
+			}
+
+			return dictionary;
+		}
+
+	}
+}
