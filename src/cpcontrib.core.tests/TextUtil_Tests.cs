6e7c608e6f12021d325933fe20edf5365c2f5eb6
Eric Newton <ericnewton76@gmail.com>
2017-12-14 17:45:43 -0500

    added TextUtil and some tests

---
+++ src/cpcontrib.core.tests/TextUtil_Tests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using datagen;
+
+using CrownPeak.CMSAPI.Custom_Library;
+
+namespace cpcontrib.core.tests
+{
+	[TestFixture]
+	public class TextUtil_Tests
+	{
+
+		const string SAMPLE_PARAGRAPH = "The quick brown fox jumps over the brown cow with zeal. Once you realize the reality is that there is no spoon.";
+		string[] _WordsList = SAMPLE_PARAGRAPH.Split(' ');
+
+		[Test]
+		public void GetWordCount_returns_correct()
+		{
+			//arrange
+			int expected = 22; //22 words in the _WordList
+			string wordstring = String.Join(" ", _WordsList);
+
+
+			//act
+			int wordCount = TextUtil.GetWordCount(wordstring);
+
+			//assert
+			wordCount.Should().Be(expected);
+		}
+
+		[Test]
+		[TestCase(SAMPLE_PARAGRAPH, 22)]
+		[TestCase("The",1)]
+		[TestCase("The quick",2)]
+		[TestCase("The quick.", 2)]
+		[TestCase("The quick. ", 2)]
+		[TestCase("The quick brown", 3)]
+		[TestCase("The quick brown.", 3)]
+		[TestCase("The quick brown. ", 3)]
+		public void GetWordCount_returns_correct(string wordstring, int expected)
+		{
+			//arrange
+			//int expected = 22; //22 words in the _WordList
+
+
+			//act
+			int wordCount = TextUtil.GetWordCount(wordstring);
+
+			//assert
+			wordCount.Should().Be(expected);
+		}
+
+		[Test]
+		[TestCase(10)]
+		[TestCase(11)]
+		[TestCase(12)]
+		[TestCase(13)]
+		[TestCase(14)]
+
+		public void WholeWordCrop(int length)
+		{
+			//arrange
+			var expected = "The quick...";
+			string wordstring = SAMPLE_PARAGRAPH;
+
+
+			//act
+			string actual = TextUtil.WholeWordCrop(wordstring, length, appendEllipsis: true);
+
+			//assert
+			actual.Should().Be(expected);
+
+		}
+
+		[Test]
+		[TestCase(10)]
+		public void WholeWordCrop_with_ellipsis(int length)
+		{
+			//arrange
+			var expected = "The quick...";
+			string wordstring = SAMPLE_PARAGRAPH;
+
+
+			//act
+			string actual = TextUtil.WholeWordCrop(wordstring, length, appendEllipsis: true);
+
+			//assert
+			actual.Should().Be(expected);
+		}
+
+		[Test]
+		[TestCase(10)]
+		public void WholeWordCrop_without_ellipsis(int length)
+		{
+			//arrange
+			var expected = "The quick";
+			string wordstring = SAMPLE_PARAGRAPH;
+
+
+			//act
+			string actual = TextUtil.WholeWordCrop(wordstring, length, appendEllipsis: false);
+
+			//assert
+			actual.Should().Be(expected);
+
+		}
+
+
+
+	}
+}
---
+++ src/cpcontrib.core/TextUtil.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownPeak.CMSAPI.Custom_Library
+{
+	public static class TextUtil
+	{
+
+		private static readonly HashSet<char> DefaultNonWordCharacters = new HashSet<char> { ',', '.', ':', ';' };
+		private static bool IsWhitespace(char character)
+		{
+			return character == ' ' || character == '\n' || character == '\t';
+		}
+
+		public static int GetWordCount(string text)
+		{
+			int wordCount = 0, index = 0;
+
+			while(index < text.Length)
+			{
+				// check if current char is part of a word
+				while(index < text.Length && IsWhitespace(text[index]) == false)
+					index++;
+
+				wordCount++;
+
+				// skip whitespace until next word
+				while(index < text.Length && IsWhitespace(text[index]))
+					index++;
+			}
+
+			return wordCount;
+		}
+
+		[Obsolete()]
+		public static string CropToWordCount(string text, int wordCount)
+		{
+			if(string.IsNullOrEmpty(text)) return "";
+
+			StringBuilder sb = new StringBuilder();
+			int foundWords = 0, index = 0;
+
+			bool whitespaceAdded = false;
+			while(index < text.Length && foundWords < wordCount)
+			{
+				// check if current char is part of a word
+				while(index < text.Length && !char.IsWhiteSpace(text[index]))
+				{
+					sb.Append(text[index]);
+					whitespaceAdded = false;
+					index++;
+				}
+
+				foundWords++;
+
+				// skip whitespace until next word
+				while(index < text.Length && char.IsWhiteSpace(text[index]))
+				{
+					if(whitespaceAdded == false) { sb.Append(text[index]); whitespaceAdded = true; }
+					index++;
+				}
+			}
+
+			return sb.ToString();
+		}
+
+
+		public static string WholeWordCrop(
+			string value, int length, bool appendEllipsis = true)
+		{
+			if(value == null)
+			{
+				throw new ArgumentNullException("value");
+			}
+
+			if(length < 0)
+			{
+				throw new ArgumentOutOfRangeException("Negative values not allowed.", "length");
+			}
+
+			var nonWordCharacters = DefaultNonWordCharacters;
+
+			if(length >= value.Length)
+			{
+				return value;
+			}
+			int end = length;
+
+			for(int i = end; i > 0; i--)
+			{
+				if(IsWhitespace(value[i]))
+				{
+					break;
+				}
+
+				if(nonWordCharacters.Contains(value[i])
+					&& (value.Length == i + 1 || value[i + 1] == ' '))
+				{
+					//Removing a character that isn't whitespace but not part 
+					//of the word either (ie ".") given that the character is 
+					//followed by whitespace or the end of the string makes it
+					//possible to include the word, so we do that.
+					break;
+				}
+				end--;
+			}
+
+			if(end == 0)
+			{
+				//If the first word is longer than the length we favor 
+				//returning it as cropped over returning nothing at all.
+				end = length;
+			}
+
+			if(appendEllipsis == false)
+				return value.Substring(0, end);
+			else
+				return value.Substring(0, end) + "...";
+
+		}
+	}
+}
