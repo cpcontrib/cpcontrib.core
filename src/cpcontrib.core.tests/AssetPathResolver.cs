fab44128e6f2cdf36d08cb30a268f802430e949b
Eric Newton <ericnewton76@gmail.com>
2017-12-14 19:13:09 -0500

    added AssetPathResolver

---
+++ src/cpcontrib.core.tests/AssetPathResolver.cs
@@ -0,0 +1,125 @@
+﻿using CrownPeak.CMSAPI.Custom_Library;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using CrownPeak.CMSAPI;
+
+namespace cpcontrib.core.tests
+{
+	[TestFixture]
+	public class AssetPathResolver_Tests
+	{
+
+		[SetUp]
+		public void SetUp()
+		{
+			AssetPathResolver = new AssetPathResolver();
+
+			AssetPathResolver.AddSpecialPath("$/Widgets", "~/_Config/Widgets");
+			AssetPathResolver.AddSpecialPath("$/Assets", (ap) => "/" + ap[0] + "/_Assets");
+			AssetPathResolver.AddSpecialPath("$/FailurePathTest", (ap) => { throw new InvalidOperationException(); });
+
+			_PathOfAsset1 = new AssetPath("/Site1/SubFolder1/Asset1");
+			_PathOfAsset1a = new AssetPath("/Site1/SubFolder1/SubFolderA/Asset1A");
+
+			_PathOfAsset2 = new AssetPath("/Site1/SubFolder1/Asset2");
+			_PathOfAsset2a = new AssetPath("/Site1/SubFolder1/SubFolder2/Asset2A");
+
+			_PathOfAsset3 = new AssetPath("/Site1/SubFolder1/SubFolder2/SubFolder3/Asset3");
+
+		}
+
+		private AssetPathResolver AssetPathResolver;
+		private AssetPath _PathOfAsset1;
+		private AssetPath _PathOfAsset1a;
+		private AssetPath _PathOfAsset2;
+		private AssetPath _PathOfAsset2a;
+		private AssetPath _PathOfAsset3;
+
+
+		[Test]
+		public void Resolve_SpecialPath_Dollar()
+		{
+			//arrange
+
+			//act
+			var actual = AssetPathResolver.Resolve(_PathOfAsset1, "$/Widgets/widget1");
+
+			//assert
+			actual.Should().Be("/Site1/_Config/Widgets/widget1");
+		}
+		[Test]
+		public void Resolve_SpecialPath_ThrowsException()
+		{
+			//arrange
+
+			//act
+			Assert.Throws<InvalidOperationException>(() =>
+			{
+				var actual = AssetPathResolver.Resolve(_PathOfAsset1, "$/FailurePathTest/this_should_fail");
+			});
+
+			//assert
+		}
+
+
+		[Test]
+		public void Resolve_Dot()
+		{
+			//arrange
+			//var resolver = AssetPathResolver;
+
+			//act
+			var actual = AssetPathResolver.Resolve(new AssetPath("/Site1/SubFolder1/Asset1"), ".");
+
+
+			//assert
+			actual.Should().Be("/Site1/SubFolder1");
+		}
+
+		[Test]
+		public void Resolve_DotDot()
+		{
+			//arrange
+			//var resolver = AssetPathResolver;
+
+			//act
+			var actual = AssetPathResolver.Resolve(new AssetPath("/Site1/SubFolder1/Asset1"), "..");
+
+
+			//assert
+			actual.Should().Be("/Site1");
+		}
+
+		[Test]
+		public void Resolve_SpecialFolder()
+		{
+			//arrange
+			//var resolver = AssetPathResolver;
+
+			//act
+			var actual = AssetPathResolver.Resolve(new AssetPath("/Site1/SubFolder1/Asset1"), "$/Assets");
+
+			//assert
+			actual.Should().Be("/Site1/_Assets");
+		}
+
+		/*
+				[Test]
+				public void TestMethod()
+				{
+					//arrange
+
+					//act
+
+					//assert
+				}
+		*/
+
+	}
+}
---
+++ src/cpcontrib.core/AssetPathResolver.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownPeak.CMSAPI.Custom_Library
+{
+
+	public class AssetPathResolver
+	{
+
+		public AssetPathStyle AssetsRootPathStyle { get; set; }
+
+		public AssetPathResolver()
+		{
+			AssetsRootPathStyle = AssetPathStyle.OrganizationLanguage;
+		}
+
+		private static AssetPathResolver S_default = new AssetPathResolver();
+		public static AssetPathResolver Default
+		{
+			get { return S_default; }
+		}
+
+		private Dictionary<string, Func<AssetPath, string>> _SpecialPaths = new Dictionary<string, Func<AssetPath, string>>();
+
+		public AssetPathResolver AddSpecialPath(string specialPathSpec, string destinationPathSpec)
+		{
+			_SpecialPaths.Add(specialPathSpec, new Func<AssetPath, string>((ap) => destinationPathSpec));
+			return this;
+		}
+		public AssetPathResolver AddSpecialPath(string specialPathSpec, Func<AssetPath, string> resolverFunction)
+		{
+			_SpecialPaths.Add(specialPathSpec, resolverFunction);
+			return this;
+		}
+
+		public string Resolve(Asset current, string assetPath)
+		{
+			return Resolve(current.AssetPath, assetPath);
+		}
+
+		public string Resolve(AssetPath current, string assetPath)
+		{
+			List<string> assetPath2 = new List<string>(assetPath.Split('/'));
+
+			if(assetPath2[0] == "$")
+			{
+				switch(AssetsRootPathStyle)
+				{
+					case AssetPathStyle.OrganizationLanguage:
+						assetPath2[0] = current[0];
+						assetPath2.Insert(1, current[1]);
+						break;
+				}
+			}
+
+			//check for special paths in the specialPaths structure.
+			foreach(string key in _SpecialPaths.Keys)
+			{
+				if(string.Compare(assetPath2[0], key, true) == 0)
+				{
+					string specialPathValue = null;
+
+					try
+					{
+						specialPathValue = _SpecialPaths[key](current);
+					}
+					catch(Exception ex)
+					{
+						throw new ApplicationException(string.Format("Failed to evaluate a path for key '{0}'.", key), ex);
+					}
+
+					List<string> assetPath3 = new List<string>(specialPathValue.Split('/'));
+
+					//replace first
+					assetPath2[0] = GetPathPart(assetPath3[1], current);
+
+					//insert all the rest
+					for(int index = 2; index < assetPath3.Count; index++)
+					{
+						assetPath2.Insert(index - 1, GetPathPart(assetPath3[index], current));
+					}
+				}
+			}
+
+			if(assetPath2[0] == "." || assetPath2[0] == "..")
+			{
+				//remember and remove the relDir specifier
+				string relDir = assetPath2[0];
+				assetPath2.RemoveAt(0);
+
+				int index = 0;
+				foreach(var part in current.Take(current.Count - relDir.Length))
+				{
+					assetPath2.Insert(index++, part);
+				}
+			}
+
+			return "/" + string.Join("/", assetPath2);
+		}
+
+		private static string GetPathPart(string part, AssetPath current)
+		{
+			int indexOfBrace = part.IndexOf("{");
+			if(indexOfBrace == -1) return part;
+
+			char[] pathChars = part.ToCharArray();
+			int braceIndexerLength = 0;
+			for(int i = indexOfBrace + 1; i < pathChars.Length; i++)
+			{
+				if((pathChars[i] >= '0' && pathChars[i] <= '9') == false) break;
+				braceIndexerLength++;
+			}
+			string pathIndexStr = part.Substring(indexOfBrace + 1, braceIndexerLength);
+
+			int pathIndex = int.Parse(pathIndexStr);
+			return current[pathIndex];
+		}
+
+	}
+
+	public enum AssetPathStyle
+	{
+		/// <summary>
+		/// a folder structure where the site's content starts in the root of the CMS instance.  Similar to: /Homepage, /Some content, /About Us/Information
+		/// </summary>
+		SiteRoot = 1
+		,
+		/// <summary>
+		/// a folder structure where the content is loaded into an Org Unit, with no language codes.  Similar to: /AdventGeneral, /Simple Site, /CompanyName, /CompanyName-West
+		/// </summary>
+		Organization = 2
+		,
+		/// <summary>
+		/// a folder structure where the content is loaded into an Org Unit first and Language after, then any assets.  Similar to: /[Organization]/[Language]/ or /AssetPath[0]/AssetPath[1]
+		/// </summary>
+		OrganizationLanguage = 3
+	}
+}
