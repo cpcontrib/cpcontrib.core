5e9d2136b5af703b57be6ceb5e03059686576bbc
Eric Newton <ericnewton76@gmail.com>
2018-01-09 00:46:56 -0500

    add Extensions for FilterParams

---
+++ src/cpcontrib.core/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//# namespaceStatement:fix
+namespace CrownPeak.CMSAPI
+{
+	//# usingStatement:fix
+	using CrownPeak.CMSAPI.Custom_Library;
+
+	public static class FilterParamsExtensions
+	{
+		public static FilterParams FilterBy(this FilterParams filterParams, TemplateRef templateRef)
+		{
+			filterParams.Add(AssetPropertyNames.TemplateId, Comparison.Equals, templateRef.TemplateId);
+			return filterParams;
+		}
+		public static FilterParams FilterBy(this FilterParams filterParams, Comparison comparison, TemplateRef templateRef)
+		{
+			filterParams.Add(AssetPropertyNames.TemplateId, comparison, templateRef.TemplateId);
+			return filterParams;
+		}
+	}
+
+}
