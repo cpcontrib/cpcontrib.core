e8d09dc2f2b7e5d1799fd77792dcfcc9532a99b9
Eric Newton <ericnewton76@gmail.com>
2018-03-02 16:10:35 -0500

    added CrownPeak.CMSAPI.AssetPath.GetParent extension method

---
+++ src/cpcontrib.core.tests/CMSAPIExtensions_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using CrownPeak.CMSAPI;
+using FluentAssertions;
+
+namespace cpcontrib.core.tests
+{
+	[TestFixture]
+	public class CMSAPIExtensions_Tests
+	{
+
+		[Test]
+		public void AssetPath_GetParent()
+		{
+			//arrange
+			AssetPath assetPath = new AssetPath("/Site1/Folder1/Folder2/Asset1");
+			var expected = "/Site1/Folder1/Folder2";
+
+			//act
+			var actual_assetpath = assetPath.GetParent();
+			var actual = actual_assetpath.ToString();
+
+			//assert
+			actual.Should().Be(expected);
+		}
+
+	}
+
+}
---
+++ src/cpcontrib.core/CMSAPIExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrownPeak.CMSAPI
+{
+	public static class CMSAPIExtensions
+	{
+
+		public static AssetPath GetParent(this AssetPath assetPath)
+		{
+			var segments = assetPath.Take(assetPath.Count - 1);
+			return new AssetPath("/"+String.Join("/", segments));
+		}
+
+	}
+}
