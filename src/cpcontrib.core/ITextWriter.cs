35612c76b3aca6c3df492e8e38392b67fc01554c
Eric Newton <ericnewton76@gmail.com>
2018-04-24 12:19:25 -0400

    Create concept of ITextWriter within the CMS, using static Out class and methods for output, but being able to substitute possibly StringBuilder in the future to capture output

---
+++ src/cpcontrib.core/ITextWriter.cs
@@ -0,0 +1,62 @@
+﻿#region /***cppack_BOF:ITextWriter.cs***/
+namespace CrownPeak.CMSAPI
+{
+	public interface ITextWriter
+	{
+		void WriteLine(string line);
+		void WriteLine(string formattedString, params object[] args);
+
+		void Write(bool value);
+		void Write(string message);
+		void Write(int value);
+		void Write(object value);
+		void Write(string format, params object[] args);
+	}
+
+}
+
+namespace CPContrib.Core
+{
+	using Out = CrownPeak.CMSAPI.Out;
+
+
+	public class OutWrapper : CrownPeak.CMSAPI.ITextWriter
+	{
+
+		public void Write(int value)
+		{
+			Out.Write(value);
+		}
+
+		public void Write(string message)
+		{
+			Out.Write(message);
+		}
+
+		public void Write(object value)
+		{
+			Out.Write(value);
+		}
+		public void Write(bool value)
+		{
+			Out.Write(value);
+		}
+
+		public void Write(string format, params object[] args)
+		{
+			Out.Write(format, args);
+		}
+
+		public void WriteLine(string line)
+		{
+			Out.WriteLine(line);
+		}
+
+		public void WriteLine(string formattedString, params object[] args)
+		{
+			Out.WriteLine(formattedString, args);
+		}
+	}
+
+}
+#endregion /***cppack_EOF:ITextWriter.cs***/
