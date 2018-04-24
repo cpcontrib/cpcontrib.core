568d967796a013b19c7223d53f80bcd8c2ecc2bb
Eric Newton <ericnewton76@gmail.com>
2018-04-24 12:18:05 -0400

    add ITemplate_* interfaces

---
+++ src/cpcontrib.core/ITemplate interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPContrib.Core
+{
+	using CrownPeak.CMSAPI;
+
+	public interface ITemplate_Output
+	{
+
+		void OnOutput(Asset asset, OutputContext context);
+	}
+
+	public interface ITemplate_Input
+	{
+		void OnInput(Asset asset, InputContext context);
+	}
+
+	public interface ITemplate_PostInput
+	{
+		void OnPostInput(Asset asset, PostInputContext context);
+
+	}
+
+	public interface ITemplate_PostSave
+	{
+		void OnPostSave(Asset asset, PostSaveContext context);
+	}
+	
+	public interface ITemplate_PostPublish
+	{
+		void OnPostPublish(Asset asset, PostPublishContext context);
+	}
+	
+}
